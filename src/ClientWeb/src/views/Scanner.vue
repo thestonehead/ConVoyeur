7e116945591e609f3e810019838a021c90d55728
Stonehead <the.stonehead@gmail.com>
2018-08-26 12:13:40 +0200

    Inital commit

---
+++ src/ClientWeb/src/views/Scanner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <video id="video" width="300" height="200" style="border: 1px solid gray"></video>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { BrowserQRCodeReader, VideoInputDevice, Result } from "@zxing/library";
+
+@Component({
+	components: {}
+})
+export default class Scanner extends Vue {
+	created() {
+		const codeReader = new BrowserQRCodeReader();
+		codeReader
+			.decodeFromInputVideoDevice(undefined, "video")
+			.then((result: Result) => console.log(result.getText()))
+			.catch(err => console.error(err));
+	}
+}
+</script>
+
+
+<style lang="scss">
+</style>
